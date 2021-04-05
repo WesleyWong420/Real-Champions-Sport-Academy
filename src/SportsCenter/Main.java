b380fc9214ddb69f4f3b0c2c10db2a61c8696af4
Wesley <>
2021-04-05 10:54:25 +0800

    Git Test Main

---
+++ src/SportsCenter/Main.java
@@ -0,0 +1,22 @@
+package SportsCenter;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
+        primaryStage.setTitle("Sports Center Home Page");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
